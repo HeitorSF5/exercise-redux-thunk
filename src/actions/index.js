37ac7cf1ae6116e5c72e6a16d97727c7d30e5f13
Heitor SF <heitor.sf5@gmail.com>
2021-10-13 20:39:37 -0300

    done

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,21 @@ export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
+export const failureAPI = () => ({type: 'FAILED_REQUEST', payload: 'error'})
+
 export function fetchAPI() {
+  // const fetchCat = await fetch('https://aws.random.cat/meow')
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+      const giveCat = await fetch('https://aws.random.cat/meow');
+      const json2Data = await giveCat.json();
+      dispatch(getPicture(json2Data));
+      
+    } catch (error) {
+      console.error(error);
+    }
+    
+  }
   // Desenvolva aqui o código da action assíncrona
 }
